156bd41ad90fb29cef3551bb5a2a2978d0148e50
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-01-30 17:30:29 -0300

    Update logo size and add flex layout to header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -6,8 +6,8 @@ import { Card, CardContent } from '../ui/card'
 export default function Header() {
   return (
     <Card>
-      <CardContent>
-        <Image src="/logo.png" alt="FSW Barber" width={22} height={120} />
+      <CardContent className="flex flex-row p-5 items-center justify-between">
+        <Image src="/logo.png" alt="FSW Barber" width={100} height={120} />
 
         <Button variant="outline" size="icon">
           <MenuIcon />
