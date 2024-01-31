ddb990cf7b76d7cc9981285162b0eb9407ecff0c
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-01-31 11:28:38 -0300

    Update Header component styling and add .postgres-data to .gitignore

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -5,12 +5,12 @@ import { Card, CardContent } from '../ui/card'
 
 export default function Header() {
   return (
-    <Card>
+    <Card className="w-auto">
       <CardContent className="flex flex-row p-5 items-center justify-between">
         <Image src="/logo.png" alt="FSW Barber" width={100} height={120} />
 
         <Button variant="outline" size="icon">
-          <MenuIcon />
+          <MenuIcon size={16} />
         </Button>
       </CardContent>
     </Card>
